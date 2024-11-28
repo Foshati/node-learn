20fd9e9eec4414ae58c4453de9269231b7b37bdf
Foshati <amirrezafoshati@gmail.com>
2024-11-28 23:42:28 +0330

    learn path fs events os

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // console.log(module);
 
-const logger = require("./logger");
+// const logger = require("./logger");
 
-logger.log("ali");
+// logger.log("ali");
+
+// const path = require("node:path")
+// const data =path.parse(__filename)
+// console.log(data)
+
+// const os= require("node:os")
+// const free = os.freemem()
+// const total=os.totalmem()
+
+// console.log(free, total)
+
+// const fs = require("node:fs");
+// const files = fs.readFileSync("./");
+// console.log(files);
+
+const EventsEmitter = require("node:events");
+const emitter = new EventsEmitter();
+
+emitter.on("msg", () => {
+  console.log("listen called");
+});
+
+emitter.emit("msg");
